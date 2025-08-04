62f8b93b0c2059dd694ca5edd0f603b6bec53e2b
Bruce Du <979659156@qq.com>
2025-08-04 10:46:44 +0800

    tow sum 2 任务打卡

--- task1/towsum/main/main.go
+++ task1/towsum/main/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	sumIndex := towSum([]int{2, 3, 45, 7}, 9)
 	fmt.Printf("sumIndex: %+v\n", sumIndex)
+	sum2 := towSum2([]int{2, 3, 5, 7}, 7)
+	fmt.Printf("sum2: %+v\n", sum2)
 }
 
 func towSum(nums []int, target int) []int {
@@ -15,6 +17,24 @@ func towSum(nums []int, target int) []int {
 				return []int{i, j}
 			}
 		}
+	}
+	return nil
+}
+
+// 每次都是从当前数向前，找补数。这样会避免找到这个元素本身
+
+func towSum2(nums []int, target int) []int {
+	numMap := make(map[int]int)
+	for i, num := range nums {
+		complement := target - num
+
+		// 判断补数，是否在map中
+		idx, found := numMap[complement]
+		if found {
+			return []int{idx, i}
+		}
+		// 将数字集合放入map中，k是 值，v是index
+		numMap[num] = i
 	}
 	return nil
 }
