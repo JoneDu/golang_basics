fd8d39320cf7eb6e6ba523135f50fc5323c80da0
Bruce Du <979659156@qq.com>
2025-08-04 08:53:18 +0800

    removeDuplicates 任务打卡2

--- task1/removeduplicates/main/main.go
+++ task1/removeduplicates/main/main.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	count := removeDuplicates([]int{1, 2, 2, 4, 5})
 	fmt.Printf("count: %+v\n", count)
+
+	duplicates1 := removeDuplicates1([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4})
+	fmt.Printf("duplicates1: %+v\n", duplicates1)
 }
 
 func removeDuplicates(nums []int) int {
@@ -18,4 +21,19 @@ label:
 		}
 	}
 	return len(nums)
+}
+
+func removeDuplicates1(nums []int) int {
+	// 使用快慢两个指针
+	if len(nums) == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
 }
